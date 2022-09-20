302597b47dcda3276264529884fdf34c5e158dd5
Youngseo Park <youngpar@c2r10s5.42seoul.kr>
2022-09-21 05:13:21 +0900

    Added: nullptr

--- nullptr.hpp
+++ nullptr.hpp
@@ -3,13 +3,13 @@
 
 namespace ft
 {
-struct nullptr_t
+const struct nullptr_t
 {
 private:
-    void operator&() const;
+    void operator&(void) const;
 public:
-    template<typename T> inline operator T*() const { return 0; }
-    template<typename T, typename U> inline operator T U::*() const { return 0; }
+    template<typename T> operator T*(void) const { return 0; }
+    template<typename T, typename U> operator T U::*() const { return 0; }
 };
 
 static nullptr_t    _nullptr = {};
--- vector.hpp
+++ vector.hpp
@@ -1,9 +1,12 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-#include "iter/iter.hpp"
+#include "iter.hpp"
 #include "nullptr.hpp"
+#include "traits.hpp"
 #include <memory>
+#include <stdexcept>
+#include <algorithm>
 
 namespace ft {
 
@@ -11,48 +14,104 @@ template <typename T, typename _Alloc = std::allocator<T> >
 class vector
 {
 public:
+	//	Type
     typedef T													value_type;
     typedef const T												const_value_type;
+	
+	//	Allocator
     typedef _Alloc												allocator_type;
     typedef typename allocator_type::reference					reference;
 	typedef typename allocator_type::const_reference			const_reference;
 	typedef typename allocator_type::pointer					pointer;
 	typedef typename allocator_type::const_pointer				const_pointer;
 
+	//	Iterator
 	typedef random_access_iterator_tag<value_type>				iterator;
 	typedef random_access_iterator_tag<const_value_type>		const_iterator;
-	typedef reverse_iterator<value_type>						reverse_iterator;	
-	typedef reverse_iterator<const_value_type>					const_reverse_iterator;
+	typedef reverse_iterator<iterator>							reverse_iterator;	
+	typedef reverse_iterator<const_iterator>					const_reverse_iterator;
 
-	typedef iterator_traits<iterator>::difference_type		difference_type;
-	typedef allocator_type::size_type							size_type;
+	//	Size
+	typedef typename iterator_traits<iterator>::difference_type	difference_type;
+	typedef typename allocator_type::size_type					size_type;
 	
-private:
-	allocator_type	_alloc;
-	pointer			_begin;
-	pointer			_end;
-	pointer			_end_cap;
+private:	//	Variable
+	allocator_type	_alloc_;
+	pointer			_begin_;
+	pointer			_end_;
+	pointer			_cap_;
 
-public:
+private:	//	Function
+	void	_init(size_type n) {
+		_begin_ = _alloc_.allocate(n);
+		_end_ = _begin_;
+		_cap_ = _begin_ + n;
+	}
+
+	void	_construct(size_type n) {
+		while (n--) _alloc_.construct(_end_++);
+	}
+	void	_construct(size_type n, T v) {
+		while (n--) _alloc_.construct(_end_++, v);
+	}
+
+	void	_destruct(size_type n) {
+		while (n-- && _end_) _alloc_.destroy(--_end_);
+	}
+	void	_destruct(pointer until) {
+		while (_end_ != until && _end_) _alloc_.destroy(--_end_);
+	}
+
+
+
+public:		//	Cannonical
 	explicit vector(const allocator_type& alloc = allocator_type())
-		: _alloc(alloc),
-		_begin(_nullptr),
-		_end(_nullptr),
-		_end_cap(_nullptr) {};
-	explicit vector(size_type n, const value_type& type = value_type(), const allocator_type& alloc = allocator_type())
-		: _alloc(alloc) {
-			_begin = _alloc.allocate(n);
-			_end = _begin;
-			_end_cap = _start + n;
-			while (n--) alloc.construct(_end++, val);
-		}
+		: _alloc_(alloc),
+		_begin_(_nullptr),
+		_end_(_nullptr),
+		_cap_(_nullptr) {};
+
+	explicit vector(size_type n,
+				const value_type& type = value_type(),
+				const allocator_type& alloc = allocator_type())
+		: _alloc_(alloc) {
+			_init(n)
+			_construct(n, type);
+		};
+
+
+	vector(const vector& v) : _alloc_(v.alloc) {
+		size_type n = v.size();	// size 구현
+		_init(v.capacity());	// capacity 구현
+		_construct(n);
+		std::copy(v._begin_, v._end_, _begin_);
+	}
 
 	template <typename InputIterator>
-	vector(InputIterator first, InputIterator second, const allocator_type& alloc = allocator_type(),
+	vector(InputIterator first,
+		InputIterator last,
+		const allocator_type& alloc = allocator_type(),
 		typename enable_if<!is_integral<InputIterator>::value>::type* = _nullptr)
-		: _alloc(alloc) {
-		
+		: _alloc_(alloc) {
+			size_type n = std::distance(first, last);
+			_init(n);
+			_construct(n);
+			std::copy(first, last, _begin_);
 		}
+
+	~vector(void) {
+		if (_begin_ == _nullptr) return ;
+		size_type pre_cap = capacity();
+		_destruct(_begin_);
+		_alloc_.deallocate(_begin_, pre_cap);
+	}
+
+	vector& operator=(const vector& v) {
+		if (this == &v)
+			return *this;
+		assign(v._begin_, v._end_);
+		return *this;
+	}
 };
 
     
